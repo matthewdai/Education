f686417be9bdfb185137bee1f306b21994b0c8ef
Minghui Dai <minghuidai@hotmail.com>
2016-04-04 16:58:40 -0400

    add new file

---
+++ Education.Data/Factory.cs
@@ -0,0 +1,23 @@
+﻿using Education.Data.Abstracts;
+using Education.Data.Concreat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education.Data
+{
+    public static class Factory
+    {
+
+        /// <summary>
+        /// Create a reporitory instance
+        /// </summary>
+        /// <returns></returns>
+        public static IQuestionsRepository GetReporitory()
+        {
+            return new QuestionsRepository();
+        }
+    }
+}
---
+++ Education.SAT.Math/Converters/ListToTextConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Education.SAT.Math.Converters
+{
+    class ListToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var list = value as IEnumerable<string>;
+
+            if (list != null)
+            {
+                return string.Join("\r", list); 
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
